465091825def96cdf1f675985a1a359ba71c967b
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2020-04-06 10:15:44 -0500

    Update QSort3.java

--- QSort3.java
+++ QSort3.java
@@ -1,3 +1,4 @@
+  
 import java.io.File;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -6,8 +7,6 @@ import java.util.stream.IntStream;
 public class QSort3 {
 	
 	public static int[] data; 
-	public static int pivot_1;
-	public static int pivot_2;
 	
 	public static void main(String[] args) {
 		data = readFiles(args[0]);
@@ -42,37 +41,39 @@ public class QSort3 {
 	}
 	
 	public static String Quicksort3(int[] data, int left, int right) {
+	
 		// handling trivial cases
-		if(data.length == 1) {
+		if(left == right) {
 			//if only one element then return
 			String result = IntStream.of(data).mapToObj(Integer::toString).collect(Collectors.joining(", "));
 			return result;
-		} else if(data.length == 2) {
+		} 
+		
+		if((left + 1) == right) {
 			//if only two elements then	compare, swap, and return
-			if(data[0] > data[1]) {
-				int temp = data[0];
-				data[0] = data[1];
-				data[1] = temp;
+			if(data[left] > data[right]) {
+				int temp = data[left];
+				data[left] = data[right];
+				data[right] = temp;
 				String result = IntStream.of(data).mapToObj(Integer::toString).collect(Collectors.joining(", "));
 				return result;
 			} else {
 				String result = IntStream.of(data).mapToObj(Integer::toString).collect(Collectors.joining(", "));
 				return result;
 			}
-		} 
-		
-		if(left >= right) {
-			String result = IntStream.of(data).mapToObj(Integer::toString).collect(Collectors.joining(", "));
-			return result;
 		}
 		
-		int n = data.length;
-		int temp = (int) Math.floor(n / 3);
+		//System.out.println("left: " + data[left]);
+		//System.out.println("right: " + data[right]);
+		
+		int temp = (int) (Math.floor((left + right) / 3));
 		int temp2 = temp + temp;
 		
 		// order pivots such that pivot_1 < pivot_2
-		pivot_1 = data[temp - 1]; //data[2] for first test array
-		pivot_2 = data[temp2 - 1]; //data[5] for first test array
+		int pivot_1 = data[temp - 1]; //data[2] for first test array
+		int pivot_2 = data[temp2 - 1]; //data[5] for first test array
+		//System.out.println("pivot1: " + pivot_1);
+		//System.out.println("pivot2: " + pivot_2);
 		
 		swap(left, pivot_1);
 		int p1 = partition_1(data, left, right, pivot_1);
@@ -82,15 +83,9 @@ public class QSort3 {
         int p2 = partition_2(data, left, right, pivot_2);
         swap(right, p2);
         
-        /*
-        if((p1 - 1) > 1) {
-        	Quicksort3(data, left, p1 - 1);
-        }
+        Quicksort3(data, left, p1 - 1);
         Quicksort3(data, p1 + 1, p2 - 1);
-        if((right - p2) > 1) {
-        	Quicksort3(data, p2 + 1, right);
-        }
-        */
+        Quicksort3(data, p2 + 1, right);
         
         String result = IntStream.of(data).mapToObj(Integer::toString).collect(Collectors.joining(", "));
 		
@@ -98,9 +93,13 @@ public class QSort3 {
 	}
 	
 	public static void swap(int position, int pivot) {
-		int tempSwap = data[position]; //sets the position value temporarily
-		data[position] = data[pivot];
-		data[pivot] = tempSwap;
+		try {
+			int tempSwap = data[position]; //sets the position value temporarily
+			data[position] = data[pivot];
+			data[pivot] = tempSwap;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static int partition_1(int[] data, int left, int right, int pivot) {
